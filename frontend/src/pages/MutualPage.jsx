d79c36f00f1c21ad9a84fb17a7845811d17203e0
subhan2108 <sk1623969@gamil.com>
2026-01-04 17:43:23 +0530

    agaitr55

--- frontend/src/pages/MutualPage.jsx
+++ frontend/src/pages/MutualPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import "./mutual.css"
 
 export default function MutualPage() {
   const [collabs, setCollabs] = useState([]);
